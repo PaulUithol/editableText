7b9878cb6df67d5aa35950b24442aafb8590c0f7
Paul Uithol <paul.uithol@gmail.com>
2013-04-02 16:32:28 +0200

    More thorough stripping of html on save.
    Partially borrowed from https://github.com/mtrimpe/jsHtmlToText .

--- jquery.editableText.js
+++ jquery.editableText.js
@@ -129,19 +129,9 @@
 			var prevValue = this.value;
 			this.value = this.element.html();
 
-			// Strip trailing '<br>' from 'value'; seems to occur (at least in FF) when typing <space>,
-			// then <enter> (even when cancelling the keydown event).
-			if ( !this.options.newlinesEnabled && this.value.match( /<br>$/ ) ) {
-				this.value = this.value.substr( 0, this.value.length - 4 );
-			}
-
 			// For Markdown, convert line breaks (FF, Chrome) and closing div tags (Chrome) to line breaks.
 			// Strip other html tags.
-			if ( this.useMarkdown ) {
-				this.value = this.value.replace( /<br>|<\/div>/gi, '\n' );
-				// Strip remaining html tags
-				this.value = this.value.replace( /<(?:.|\n)*?>/gm, '' );
-			}
+			this.value = this._htmlToText( this.value );
 
 			this._setContent( this.value );
 			
@@ -230,6 +220,32 @@
 			if ( !target.closest( this.element ).length && !target.closest( this.buttons ).length ) {
 				this.save( ev );
 			}
+		},
+
+		_htmlToText: function( html ) {
+			html = html
+				// Remove line breaks
+				.replace( /(?:\n|\r\n|\r)/ig, ' ' )
+				// Decode HTML entities.
+				.replace( /&([^;]+);/g, decodeHtmlEntity )
+				// Remove content in script tags.
+				.replace( /<\s*script[^>]*>[\s\S]*?<\/script>/mig, '' )
+				// Remove content in style tags.
+				.replace( /<\s*style[^>]*>[\s\S]*?<\/style>/mig, '' )
+				// Remove content in comments.
+				.replace( /<!--.*?-->/mig, '' )
+				// Remove !DOCTYPE
+				.replace( /<!DOCTYPE.*?>/ig, '' )
+				// Convert tags that should display a line break to `\n`
+				.replace( /<\s*br[^>]*\/?\s*>|<\/div>|<\/p>/gi, '\n' )
+				// Strip remaining html tags
+				.replace( /<(?:.|\n)*?>/gm, '' )
+				// Remove newlines at the beginning of the text.
+				.replace( /^\n+/, '' )
+				// Remove newlines at the end of the text.
+				.replace( /\n+$/, '' );
+
+			return html;
 		}
 	};
 	
@@ -315,5 +331,166 @@
 		change: null,
 		startEditing: null,
 		stopEditing: null
+	};
+
+	function decodeHtmlEntity( m, n ) {
+		// Determine the character code of the entity. Range is 0 to 65535
+		// (characters in JavaScript are Unicode, and entities can represent
+		// Unicode characters).
+		var code;
+
+		// Try to parse as numeric entity. This is done before named entities for
+		// speed because associative array lookup in many JavaScript implementations
+		// is a linear search.
+		if ( n.substr( 0, 1 ) == '#' ) {
+			// Try to parse as numeric entity
+			if ( n.substr( 1, 1 ) == 'x' ) {
+				// Try to parse as hexadecimal
+				code = parseInt( n.substr( 2 ), 16 );
+			}
+			else {
+				// Try to parse as decimal
+				code = parseInt( n.substr( 1 ), 10 );
+			}
+		}
+		else {
+			// Try to parse as named entity
+			code = ENTITIES_MAP[n];
+		}
+
+		// If still nothing, pass entity through
+		return ( code === undefined || isNaN( code ) ) ?
+			'&' + n + ';' : String.fromCharCode(code);
+	}
+
+	var ENTITIES_MAP = {
+		'quot' : 34,
+		'amp' : 38,
+		'lt' : 60,
+		'gt' : 62,
+		'nbsp' : 160,
+		'iexcl' : 161,
+		'cent' : 162,
+		'pound' : 163,
+		'curren' : 164,
+		'yen' : 165,
+		'brvbar' : 166,
+		'sect' : 167,
+		'uml' : 168,
+		'copy' : 169,
+		'ordf' : 170,
+		'laquo' : 171,
+		'not' : 172,
+		'shy' : 173,
+		'reg' : 174,
+		'macr' : 175,
+		'deg' : 176,
+		'plusmn' : 177,
+		'sup2' : 178,
+		'sup3' : 179,
+		'acute' : 180,
+		'micro' : 181,
+		'para' : 182,
+		'middot' : 183,
+		'cedil' : 184,
+		'sup1' : 185,
+		'ordm' : 186,
+		'raquo' : 187,
+		'frac14' : 188,
+		'frac12' : 189,
+		'frac34' : 190,
+		'iquest' : 191,
+		'Agrave' : 192,
+		'Aacute' : 193,
+		'Acirc' : 194,
+		'Atilde' : 195,
+		'Auml' : 196,
+		'Aring' : 197,
+		'AElig' : 198,
+		'Ccedil' : 199,
+		'Egrave' : 200,
+		'Eacute' : 201,
+		'Ecirc' : 202,
+		'Euml' : 203,
+		'Igrave' : 204,
+		'Iacute' : 205,
+		'Icirc' : 206,
+		'Iuml' : 207,
+		'ETH' : 208,
+		'Ntilde' : 209,
+		'Ograve' : 210,
+		'Oacute' : 211,
+		'Ocirc' : 212,
+		'Otilde' : 213,
+		'Ouml' : 214,
+		'times' : 215,
+		'Oslash' : 216,
+		'Ugrave' : 217,
+		'Uacute' : 218,
+		'Ucirc' : 219,
+		'Uuml' : 220,
+		'Yacute' : 221,
+		'THORN' : 222,
+		'szlig' : 223,
+		'agrave' : 224,
+		'aacute' : 225,
+		'acirc' : 226,
+		'atilde' : 227,
+		'auml' : 228,
+		'aring' : 229,
+		'aelig' : 230,
+		'ccedil' : 231,
+		'egrave' : 232,
+		'eacute' : 233,
+		'ecirc' : 234,
+		'euml' : 235,
+		'igrave' : 236,
+		'iacute' : 237,
+		'icirc' : 238,
+		'iuml' : 239,
+		'eth' : 240,
+		'ntilde' : 241,
+		'ograve' : 242,
+		'oacute' : 243,
+		'ocirc' : 244,
+		'otilde' : 245,
+		'ouml' : 246,
+		'divide' : 247,
+		'oslash' : 248,
+		'ugrave' : 249,
+		'uacute' : 250,
+		'ucirc' : 251,
+		'uuml' : 252,
+		'yacute' : 253,
+		'thorn' : 254,
+		'yuml' : 255,
+		'OElig' : 338,
+		'oelig' : 339,
+		'Scaron' : 352,
+		'scaron' : 353,
+		'Yuml' : 376,
+		'circ' : 710,
+		'tilde' : 732,
+		'ensp' : 8194,
+		'emsp' : 8195,
+		'thinsp' : 8201,
+		'zwnj' : 8204,
+		'zwj' : 8205,
+		'lrm' : 8206,
+		'rlm' : 8207,
+		'ndash' : 8211,
+		'mdash' : 8212,
+		'lsquo' : 8216,
+		'rsquo' : 8217,
+		'sbquo' : 8218,
+		'ldquo' : 8220,
+		'rdquo' : 8221,
+		'bdquo' : 8222,
+		'dagger' : 8224,
+		'Dagger' : 8225,
+		'permil' : 8240,
+		'lsaquo' : 8249,
+		'rsaquo' : 8250,
+		'euro' : 8364
 	};
 })( jQuery );
